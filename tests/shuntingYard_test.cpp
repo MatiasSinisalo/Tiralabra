7e20e92daf07064e754f9a946a369dabc6d31876
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-09-16 17:23:23 +0300

    write tests to check some multiply behauvior for followCountingRules

--- tests/shuntingYard_test.cpp
+++ tests/shuntingYard_test.cpp
@@ -191,6 +191,14 @@ void checkFirstTokenIsCorrectOperator(vector<token> tokens, operatorType expecte
     EXPECT_EQ(tokens[tokens.size() - 1].opType, expectedOperatorType);
 }
 
+void checkOperatorsAreCorrect(vector<token> tokens, vector<operatorType> expectedOperators){
+    EXPECT_EQ(tokens.size(), expectedOperators.size());
+    for(int i = 0; i < tokens.size(); i++){
+        EXPECT_EQ(tokens[i].type, OPERATOR);
+        EXPECT_EQ(tokens[i].opType, expectedOperators[i]);
+    }
+}
+
 TEST(shuntingYardTests_followCountingRules, PlusPlusCausesPlusToEnterOutput){
     vector<token> output = {};
     vector<token> operators = {};
@@ -220,5 +228,33 @@ TEST(shuntingYardTests_followCountingRules, PlusMinusCausesPlusToEnterOutput){
     followCountingRules(output, operators, {.type = OPERATOR, .opType = MINUS, .numberVal = 0});
     checkFirstTokenIsCorrectOperator(output, PLUS, 1);
     checkFirstTokenIsCorrectOperator(operators, MINUS, 1);
+}
 
+TEST(shuntingYardTests_followCountingRules, plusMultiplyStaysOnOperators){
+    vector<token> output = {};
+    vector<token> operators = {};
+
+
+    followCountingRules(output, operators, {.type = OPERATOR, .opType = PLUS, .numberVal = 0});
+    EXPECT_EQ(output.size(), 0);
+    checkFirstTokenIsCorrectOperator(operators, PLUS, 1);
+    
+    followCountingRules(output, operators, {.type = OPERATOR, .opType = MULTIPLY, .numberVal = 0});
+    EXPECT_EQ(output.size(), 0);
+    
+    checkOperatorsAreCorrect(operators, {PLUS, MULTIPLY});
+}
+
+TEST(shuntingYardTests_followCountingRules, multiplyPlusCausesMultiplyToEnterOutput){
+    vector<token> output = {};
+    vector<token> operators = {};
+
+
+    followCountingRules(output, operators, {.type = OPERATOR, .opType = MULTIPLY, .numberVal = 0});
+    EXPECT_EQ(output.size(), 0);
+    checkFirstTokenIsCorrectOperator(operators, MULTIPLY, 1);
+    
+    followCountingRules(output, operators, {.type = OPERATOR, .opType = PLUS, .numberVal = 0});
+    checkOperatorsAreCorrect(output, {MULTIPLY});
+    checkOperatorsAreCorrect(operators, {PLUS});
 }
