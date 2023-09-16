a5290443a52b20bb9525aeb256da4e9e40ba3aff
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-09-16 13:17:41 +0300

    write tests for getTokensFromInputString

--- tests/tokenizer_test.cpp
+++ tests/tokenizer_test.cpp
@@ -15,3 +15,132 @@ TEST(tokenizerTests, tokenizerDetectsNumbers) {
 	EXPECT_EQ(tokens[0].opType, expectedToken.opType);
 	EXPECT_EQ(tokens[0].numberVal, expectedToken.numberVal);
 };
+
+TEST(tokenizerTests, tokenizerDetectsPlusOperator){
+	string input = "+";
+	vector<token> tokens = getTokensFromInputString(input);
+	EXPECT_EQ(tokens.size(), 1);
+
+	token expectedToken = {
+		.type = OPERATOR,
+		.opType = PLUS,
+		.numberVal = 0
+	};
+
+	EXPECT_EQ(tokens[0].type, expectedToken.type);
+	EXPECT_EQ(tokens[0].opType, expectedToken.opType);
+	EXPECT_EQ(tokens[0].numberVal, expectedToken.numberVal);
+};
+
+
+TEST(tokenizerTests, tokenizerDetectsMinusOperator){
+	string input = "-";
+	vector<token> tokens = getTokensFromInputString(input);
+	EXPECT_EQ(tokens.size(), 1);
+
+	token expectedToken = {
+		.type = OPERATOR,
+		.opType = MINUS,
+		.numberVal = 0
+	};
+
+	EXPECT_EQ(tokens[0].type, expectedToken.type);
+	EXPECT_EQ(tokens[0].opType, expectedToken.opType);
+	EXPECT_EQ(tokens[0].numberVal, expectedToken.numberVal);
+};
+
+
+TEST(tokenizerTests, tokenizerDetectsMultiplyOperator){
+	string input = "*";
+	vector<token> tokens = getTokensFromInputString(input);
+	EXPECT_EQ(tokens.size(), 1);
+
+	token expectedToken = {
+		.type = OPERATOR,
+		.opType = MULTIPLY,
+		.numberVal = 0
+	};
+
+	EXPECT_EQ(tokens[0].type, expectedToken.type);
+	EXPECT_EQ(tokens[0].opType, expectedToken.opType);
+	EXPECT_EQ(tokens[0].numberVal, expectedToken.numberVal);
+};
+
+TEST(tokenizerTests, tokenizerDetectsDivideOperator){
+	string input = "/";
+	vector<token> tokens = getTokensFromInputString(input);
+	EXPECT_EQ(tokens.size(), 1);
+
+	token expectedToken = {
+		.type = OPERATOR,
+		.opType = DIVIDE,
+		.numberVal = 0
+	};
+
+	EXPECT_EQ(tokens[0].type, expectedToken.type);
+	EXPECT_EQ(tokens[0].opType, expectedToken.opType);
+	EXPECT_EQ(tokens[0].numberVal, expectedToken.numberVal);
+};
+
+TEST(tokenizerTests, tokenizerDetectsNumbersAndOperators){
+	string input = "1+2-3*4/5";
+	vector<token> tokens = getTokensFromInputString(input);
+	EXPECT_EQ(tokens.size(), 9);
+
+
+	vector<token> expectedTokens = {
+		{
+			.type = NUMBER,
+			.opType = NONE,
+			.numberVal = 1
+		},
+		{
+			.type = OPERATOR,
+			.opType = PLUS,
+			.numberVal = 0
+		},
+		{
+			.type = NUMBER,
+			.opType = NONE,
+			.numberVal = 2
+		},
+		{
+			.type = OPERATOR,
+			.opType = MINUS,
+			.numberVal = 0
+		},
+		{
+			.type = NUMBER,
+			.opType = NONE,
+			.numberVal = 3
+		},
+		{
+			.type = OPERATOR,
+			.opType = MULTIPLY,
+			.numberVal = 0
+		},
+		{
+			.type = NUMBER,
+			.opType = NONE,
+			.numberVal = 4
+		},
+		{
+			.type = OPERATOR,
+			.opType = DIVIDE,
+			.numberVal = 0
+		},
+		{
+			.type = NUMBER,
+			.opType = NONE,
+			.numberVal = 5
+		}
+	};
+	
+	for(int i = 0; i < tokens.size(); i++){
+		EXPECT_EQ(tokens[i].type, expectedTokens[i].type);
+		EXPECT_EQ(tokens[i].opType, expectedTokens[i].opType);
+		EXPECT_EQ(tokens[i].numberVal, expectedTokens[i].numberVal);
+	}
+	
+};
+
