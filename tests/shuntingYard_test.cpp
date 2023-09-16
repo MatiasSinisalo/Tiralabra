b4e8417925bb003fe5678569ad55cbaee52de57b
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-09-16 17:35:38 +0300

    write some tests for shuntingYard function

--- tests/shuntingYard_test.cpp
+++ tests/shuntingYard_test.cpp
@@ -199,6 +199,15 @@ void checkOperatorsAreCorrect(vector<token> tokens, vector<operatorType> expecte
     }
 }
 
+void checkTokensMatch(vector<token> tokensA, vector<token> tokensB){
+    EXPECT_EQ(tokensA.size(), tokensB.size());
+    for(int i = 0; i < tokensA.size(); i++){
+        EXPECT_EQ(tokensA[i].type, tokensB[i].type);
+        EXPECT_EQ(tokensA[i].opType, tokensB[i].opType);
+        EXPECT_EQ(tokensA[i].numberVal, tokensB[i].numberVal);
+    }
+}
+
 TEST(shuntingYardTests_followCountingRules, PlusPlusCausesPlusToEnterOutput){
     vector<token> output = {};
     vector<token> operators = {};
@@ -257,4 +266,148 @@ TEST(shuntingYardTests_followCountingRules, multiplyPlusCausesMultiplyToEnterOut
     followCountingRules(output, operators, {.type = OPERATOR, .opType = PLUS, .numberVal = 0});
     checkOperatorsAreCorrect(output, {MULTIPLY});
     checkOperatorsAreCorrect(operators, {PLUS});
+}
+
+TEST(shuntingYardTests_shuntingYard, producesExpectedOutputFor1Plus1Minus2){
+    vector<token> input = {
+        {
+            .type = NUMBER,
+            .opType = NONE,
+            .numberVal = 1,
+        },
+        {
+            .type = OPERATOR,
+            .opType = PLUS,
+            .numberVal = 0,
+        },
+        {
+            .type = NUMBER,
+            .opType = NONE,
+            .numberVal = 1,
+        },
+        {
+            .type = OPERATOR,
+            .opType = MINUS,
+            .numberVal = 0,
+        },
+        {
+            .type = NUMBER,
+            .opType = NONE,
+            .numberVal = 2,
+        },
+    };
+
+    vector<token> expected_output = {
+        {
+            .type = NUMBER,
+            .opType = NONE,
+            .numberVal = 1,
+        },
+        {
+            .type = NUMBER,
+            .opType = NONE,
+            .numberVal = 1,
+        },
+        {
+            .type = OPERATOR,
+            .opType = PLUS,
+            .numberVal = 0,
+        },
+        {
+            .type = NUMBER,
+            .opType = NONE,
+            .numberVal = 2,
+        },
+        {
+            .type = OPERATOR,
+            .opType = MINUS,
+            .numberVal = 0,
+        },
+    };
+    
+    vector<token> output = shuntingYard(input);
+
+    checkTokensMatch(output, expected_output);
+}
+
+TEST(shuntingYardTests_shuntingYard, producesExpectedOutputForOnePlus2Plus3Times4){
+    vector<token> input = {
+        {
+            .type = NUMBER,
+            .opType = NONE,
+            .numberVal = 1,
+        },
+        {
+            .type = OPERATOR,
+            .opType = PLUS,
+            .numberVal = 0,
+        },
+        {
+            .type = NUMBER,
+            .opType = NONE,
+            .numberVal = 2,
+        },
+        {
+            .type = OPERATOR,
+            .opType = PLUS,
+            .numberVal = 0,
+        },
+        {
+            .type = NUMBER,
+            .opType = NONE,
+            .numberVal = 3,
+        },
+        {
+            .type = OPERATOR,
+            .opType = MULTIPLY,
+            .numberVal = 0,
+        },
+        {
+            .type = NUMBER,
+            .opType = NONE,
+            .numberVal = 4,
+        },
+    };
+
+    vector<token> expected_output = {
+        {
+            .type = NUMBER,
+            .opType = NONE,
+            .numberVal = 1,
+        },
+        {
+            .type = NUMBER,
+            .opType = NONE,
+            .numberVal = 2,
+        },
+        {
+            .type = OPERATOR,
+            .opType = PLUS,
+            .numberVal = 0,
+        },
+       {
+            .type = NUMBER,
+            .opType = NONE,
+            .numberVal = 3,
+        },
+        {
+            .type = NUMBER,
+            .opType = NONE,
+            .numberVal = 4,
+        },
+        {
+            .type = OPERATOR,
+            .opType = MULTIPLY,
+            .numberVal = 0,
+        },
+        {
+            .type = OPERATOR,
+            .opType = PLUS,
+            .numberVal = 0,
+        },
+    };
+    
+    vector<token> output = shuntingYard(input);
+
+    checkTokensMatch(output, expected_output);
 }
