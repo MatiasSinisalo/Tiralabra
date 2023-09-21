0e3463c9f7507b509665fc75bf7f10adb227d253
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-09-21 15:14:23 +0300

    remove hello.cpp and hello.h

--- include/hello.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-using namespace std;
-
-
-void say_hello();
-void thisFunctionIsNotTested();
-
--- src/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include "hello.h"
-
-void say_hello(){
-    cout << "the program says hello!\n";
-}
-
-void thisFunctionIsNotTested(){
-    cout << "hello from insecure function";
-}
