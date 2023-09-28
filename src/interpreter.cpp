f4b7b1350dfbf06eab6c61a4abfe16a6cfd1756f
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-09-28 15:35:14 +0300

    fix multiple redefinitions in interpreter

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -1,68 +1,91 @@
 #include "interpreter.h"
 
 token interpretFromRPN(const vector<token> tokensInRPN){
-    token nextOperation = {.opType = NONE};
     vector<token> helperStack;
     //this loop does not go though the operators like they would be in a stack...
     //should go in reverse order of the vector.
     for(int i = 0; i< tokensInRPN.size(); i++){
-        if(tokensInRPN[i].type == NUMBER){
+
+        switch (tokensInRPN[i].type)
+        {
+        case NUMBER:
             helperStack.push_back(tokensInRPN[i]);
-        }
-        else if(tokensInRPN[i].type == OPERATOR){
+            break;
+        case OP_PLUS:
+        {
+            token evaluatedToken;
+            evaluatedToken.type = NUMBER;
+
             token firstToken = helperStack[helperStack.size() - 1];
             helperStack.pop_back();
-
             token secondToken = helperStack[helperStack.size() - 1];
             helperStack.pop_back();
 
-            
+            evaluatedToken.numberVal = secondToken.numberVal + firstToken.numberVal;
+            helperStack.push_back(evaluatedToken);
+            break;
+        }
+        case OP_MINUS:
+        {
             token evaluatedToken;
             evaluatedToken.type = NUMBER;
-            evaluatedToken.opType = NONE;
-            switch (tokensInRPN[i].opType)
-            {
-            case PLUS:
-                evaluatedToken.numberVal = secondToken.numberVal + firstToken.numberVal;
-                helperStack.push_back(evaluatedToken);
-                break;
-            case MINUS:
-                evaluatedToken.numberVal = secondToken.numberVal - firstToken.numberVal;
-                helperStack.push_back(evaluatedToken);
-                break;
-            case MULTIPLY:
-                evaluatedToken.numberVal = secondToken.numberVal * firstToken.numberVal;
-                helperStack.push_back(evaluatedToken);
-                break;
-            case DIVIDE:
-                evaluatedToken.numberVal = secondToken.numberVal / firstToken.numberVal;
-                helperStack.push_back(evaluatedToken);
-                break;
-            default:
-                break;
-            }
-        }
-        else if (tokensInRPN[i].type == FUNCTION) {
+
             token firstToken = helperStack[helperStack.size() - 1];
             helperStack.pop_back();
-
             token secondToken = helperStack[helperStack.size() - 1];
             helperStack.pop_back();
 
-
+            evaluatedToken.numberVal = secondToken.numberVal - firstToken.numberVal;
+            helperStack.push_back(evaluatedToken);
+            break;
+        }
+        case OP_MULTIPLY:
+        {
             token evaluatedToken;
             evaluatedToken.type = NUMBER;
-            evaluatedToken.opType = NONE;
-            switch (tokensInRPN[i].functionType == TO_POWER_OF)
-            {
-            case TO_POWER_OF:
-                evaluatedToken.numberVal = pow(secondToken.numberVal, firstToken.numberVal);
-                helperStack.push_back(evaluatedToken);
-                break;
-            default:
-                break;
-            }
+
+            token firstToken = helperStack[helperStack.size() - 1];
+            helperStack.pop_back();
+            token secondToken = helperStack[helperStack.size() - 1];
+            helperStack.pop_back();
+
+            evaluatedToken.numberVal = secondToken.numberVal * firstToken.numberVal;
+            helperStack.push_back(evaluatedToken);
+            break;
         }
+        case OP_DIVIDE:
+        {
+            token evaluatedToken;
+            evaluatedToken.type = NUMBER;
+
+            token firstToken = helperStack[helperStack.size() - 1];
+            helperStack.pop_back();
+            token secondToken = helperStack[helperStack.size() - 1];
+            helperStack.pop_back();
+
+            evaluatedToken.numberVal = secondToken.numberVal / firstToken.numberVal;
+            helperStack.push_back(evaluatedToken);
+            break;
+        }
+        case FUNC_POWER:
+        {
+            token evaluatedToken;
+            evaluatedToken.type = NUMBER;
+
+            token firstToken = helperStack[helperStack.size() - 1];
+            helperStack.pop_back();
+            token secondToken = helperStack[helperStack.size() - 1];
+            helperStack.pop_back();
+
+            evaluatedToken.numberVal = pow(secondToken.numberVal, firstToken.numberVal);
+            helperStack.push_back(evaluatedToken);
+            break;
+        }
+        default:
+            break;
+        }
+        
+      
     }
 
     if(helperStack.size() > 1){
