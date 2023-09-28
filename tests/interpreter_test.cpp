ad3f21bbc5293e785d38c6c77b045c87ed5531f4
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-09-28 17:05:15 +0300

    Update interpreter_test.cpp

--- tests/interpreter_test.cpp
+++ tests/interpreter_test.cpp
@@ -5,114 +5,113 @@
 void testInterpreterFor(vector<token> tokens, token expectedOutput){
     token output = interpretFromRPN(tokens);
     ASSERT_EQ(output.type, expectedOutput.type);
-    ASSERT_EQ(output.opType, expectedOutput.opType);
     ASSERT_EQ(output.numberVal, expectedOutput.numberVal);
 }
 
 TEST(InterpreterTests, interpretFromRPNReturnsCorrectResultForOnePlusOne){
     vector<token> tokens = {
         {
+             
             .type = NUMBER,
-            .opType = NONE,
             .numberVal = 1
         },
         {
+             
             .type = NUMBER,
-            .opType = NONE,
             .numberVal = 1
         },
         {
-            .type = OPERATOR,
-            .opType = PLUS,
+             
+            .type = OP_PLUS,
             .numberVal = 0
         }
     };
 
-    testInterpreterFor(tokens, {.type = NUMBER, .opType = NONE, .numberVal = 2});
+    testInterpreterFor(tokens, {  .type = NUMBER, .numberVal = 2});
 }
 
 TEST(InterpreterTests, interpretFromRPNReturnsCorrectResultForTwoMinusOne){
     vector<token> tokens = {
         {
+             
             .type = NUMBER,
-            .opType = NONE,
             .numberVal = 2
         },
         {
+             
             .type = NUMBER,
-            .opType = NONE,
             .numberVal = 1
         },
         {
-            .type = OPERATOR,
-            .opType = MINUS,
+             
+            .type = OP_MINUS,
             .numberVal = 0
         }
     };
 
-    testInterpreterFor(tokens, {.type = NUMBER, .opType = NONE, .numberVal = 1});
+    testInterpreterFor(tokens, {  .type = NUMBER, .numberVal = 1});
 }
 
 TEST(InterpreterTests, interpretFromRPNReturnsCorrectResultForOnePlusTwoTimesFive){
     vector<token> tokens = {
         {
+             
             .type = NUMBER,
-            .opType = NONE,
             .numberVal = 1
         },
         {
+             
             .type = NUMBER,
-            .opType = NONE,
             .numberVal = 2
         },
         {
+             
             .type = NUMBER,
-            .opType = NONE,
             .numberVal = 5
         },
         {
-            .type = OPERATOR,
-            .opType = MULTIPLY,
+             
+            .type = OP_MULTIPLY,
             .numberVal = 0
         },
         {
-            .type = OPERATOR,
-            .opType = PLUS,
+             
+            .type = OP_PLUS,
             .numberVal = 0
         }
     };
 
-    testInterpreterFor(tokens, {.type = NUMBER, .opType = NONE, .numberVal = 11});
+    testInterpreterFor(tokens, {  .type = NUMBER, .numberVal = 11});
 }
 
 TEST(InterpreterTests, interpretFromRPNReturnsCorrectResultForTwoMinusFourDividedBy2){
     vector<token> tokens = {
         {
+             
             .type = NUMBER,
-            .opType = NONE,
             .numberVal = 2
         },
         {
+             
             .type = NUMBER,
-            .opType = NONE,
             .numberVal = 4
         },
         {
+             
             .type = NUMBER,
-            .opType = NONE,
             .numberVal = 2
         },
         {
-            .type = OPERATOR,
-            .opType = DIVIDE,
+             
+            .type = OP_DIVIDE,
             .numberVal = 0
         },
         {
-            .type = OPERATOR,
-            .opType = MINUS,
+             
+            .type = OP_MINUS,
             .numberVal = 0
         }
     };
 
-    testInterpreterFor(tokens, {.type = NUMBER, .opType = NONE, .numberVal = 0});
+    testInterpreterFor(tokens, {  .type = NUMBER, .numberVal = 0});
 }
