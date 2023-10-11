2334370d65fffe0213741692ee3ed72fce44367b
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-10-11 13:47:47 +0300

    write intepreter test for multiple SET_CUSTOM_FUNCTION on the same function

--- tests/interpreter_test.cpp
+++ tests/interpreter_test.cpp
@@ -318,3 +318,80 @@ TEST(InterpreterTests, interpretFromRPNReturnsCorrectResultForSET_FUNCTION) {
 }
 
 
+TEST(InterpreterTests, interpretFromRPNReturnsCorrectResultForMultipleSET_FUNCTIONOnTheSameFunction) {
+    tokenData data = {
+
+        .functionStringToID = {{"foo", 1}},
+        .functionExpressions = {
+                {1,
+                    {}
+                }
+            }
+    };
+
+    vector<token> firstInputTokens = {
+        {
+            .type = CUSTOM_FUNCTION,
+            .value = 1
+        },
+        {
+            .type = NUMBER,
+            .value = 1
+        },
+        {
+            .type = NUMBER,
+            .value = 1
+        },
+        {
+            .type = OP_PLUS
+        },
+        {
+            .type = FUNC_SET_CUSTOM_FUNCTION,
+            .value = 1
+        }
+    };
+
+    testInterpreterFor(firstInputTokens, { .type = NUMBER, .value = 2 }, data);
+
+
+    vector<token> secondInputTokens = {
+        {
+            .type = CUSTOM_FUNCTION,
+            .value = 1
+        },
+        {
+            .type = NUMBER,
+            .value = 5
+        },
+        {
+            .type = NUMBER,
+            .value = 5
+        },
+        {
+            .type = OP_MULTIPLY
+        },
+        {
+            .type = FUNC_SET_CUSTOM_FUNCTION,
+            .value = 1
+        }
+    };
+    testInterpreterFor(firstInputTokens, { .type = NONE, .value = 0 }, data);
+
+    ASSERT_EQ(data.functionStringToID.size(), 1);
+    ASSERT_EQ(data.functionStringToID["foo"], 1);
+    checkTokensMatch(data.functionExpressions[1], {
+        {
+            .type = NUMBER,
+            .value = 1
+        },
+        {
+            .type = NUMBER,
+            .value = 1
+        },
+        {
+            .type = OP_PLUS
+        } });
+
+}
+
+
